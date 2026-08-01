0d58cbe0cf5e5306e1de29d5388195bc6848b856
Maksym Mykolyshyn <maksymmykolyshyn@gmail.com>
2026-08-01 21:58:16 +0300

    fixing params

--- app/[locale]/movie/[id]/page.tsx
+++ app/[locale]/movie/[id]/page.tsx
@@ -6,13 +6,14 @@ import HeaderMain from "@/components/layout/header/HeaderMain";
 type MoviePageProps = {
   params: Promise<{
     id: string;
+    locale: string;
   }>;
 };
 
 export default async function MoviePage({ params }: MoviePageProps) {
-  const { id } = await params;
+  const { id, locale } = await params;
 
-  const movie = await getMovie(+id);
+  const movie = await getMovie(+id, locale);
 
   return (
     <>
